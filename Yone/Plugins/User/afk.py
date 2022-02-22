363594f4691bb9560146c400a716ad881b22e5df
AnvaUserBot <98270034+shakoHSYNV@users.noreply.github.com>
2022-02-22 23:59:53 +0400

    Update afk.py

--- Yone/Plugins/User/afk.py
+++ Yone/Plugins/User/afk.py
@@ -1,12 +1,12 @@
 import random, html
 
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.disable import (
+from Yone import dispatcher
+from Yone.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from LaylaRobot.modules.sql import afk_sql as sql
-from LaylaRobot.modules.users import get_user_id
+from YoneRobot.modules.sql import afk_sql as sql
+from YoneRobot.modules.users import get_user_id
 from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
